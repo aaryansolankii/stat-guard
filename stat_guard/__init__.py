3e795b24805315d7bc2e19f23c0a91742ccc0be7
Aaryan Solanki <135635063+aaryansolankii@users.noreply.github.com>
2026-02-21 14:41:06 +0530

    Update __init__.py

--- stat_guard/__init__.py
+++ stat_guard/__init__.py
@@ -26,7 +26,7 @@ from .profilers.data_profiler import DatasetProfile
 from .policy import ValidationPolicy
 from .engine import ValidationError
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "validate",
